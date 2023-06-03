8b985322bd73ea03dda10ac246582871cedb8fc8
Mishka Mebonia <nini@Mishkas-MacBook-Pro.local>
2023-06-04 03:25:42 +0400

    finished brand and add lists

--- scripts/quantityProduct.js
+++ scripts/quantityProduct.js
@@ -43,6 +43,16 @@ function categoryQuantity() {
 }
 
 export function brandQuantity() {
+  const showMore = document.querySelector('#view-more')
+  const hiddenContent = document.querySelector('#hidden-content')
+  const dropDown = document.querySelector('.view-more-dropdown')
+
+  showMore.addEventListener('click', () => {
+    hiddenContent.classList.toggle('visible')
+    dropDown.classList.toggle('fa-chevron-down')
+    dropDown.classList.toggle('fa-chevron-up')
+  })
+
   const apple = document.querySelector('#apple-quantity')
   const samsung = document.querySelector('#samsung-quantity')
   const hp = document.querySelector('#hp-quantity')
@@ -53,8 +63,24 @@ export function brandQuantity() {
   const lg = document.querySelector('#lg-quantity')
   const oneplus = document.querySelector('#oneplus-quantity')
   const acer = document.querySelector('#acer-quantity')
+  const microsoft = document.querySelector('#microsoft-quantity')
+  const alienware = document.querySelector('#alienware-quantity')
+  const xiaomi = document.querySelector('#xiaomi-quantity')
+  const razer = document.querySelector('#razer-quantity')
+  const msi = document.querySelector('#msi-quantity')
+  const nvidia = document.querySelector('#nvidia-quantity')
+  const bose = document.querySelector('#bose-quantity')
+  const gopro = document.querySelector('#gopro-quantity')
+  const fitbit = document.querySelector('#fitbit-quantity')
+  const canon = document.querySelector('#canon-quantity')
+  const nintendo = document.querySelector('#nintendo-quantity')
+  const sonos = document.querySelector('#sonos-quantity')
+  const dji = document.querySelector('#dji-quantity')
+  const amazon = document.querySelector('#amazon-quantity')
+  const asus = document.querySelector('#asus-quantity')
+  const jbl = document.querySelector('#jbl-quantity')
 
-  const brandIds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  const brandIds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
 
   const counts = {}
   brandIds.forEach(brandId => {
@@ -67,14 +93,30 @@ export function brandQuantity() {
     counts[brandId] = count
   })
 
-  apple.innerHTML = counts[1] || 0
-  samsung.innerHTML = counts[2] || 0
-  hp.innerHTML = counts[3] || 0
-  dell.innerHTML = counts[4] || 0
-  sony.innerHTML = counts[5] || 0
-  lenovo.innerHTML = counts[6] || 0
-  google.innerHTML = counts[7] || 0
-  lg.innerHTML = counts[8] || 0
-  oneplus.innerHTML = counts[9] || 0
-  acer.innerHTML = counts[10] || 0
+  samsung.innerHTML = counts[1] || 0
+  apple.innerHTML = counts[2] || 0
+  dell.innerHTML = counts[3] || 0
+  hp.innerHTML = counts[4] || 0
+  lg.innerHTML = counts[5] || 0
+  sony.innerHTML = counts[6] || 0
+  lenovo.innerHTML = counts[7] || 0
+  google.innerHTML = counts[8] || 0
+  acer.innerHTML = counts[9] || 0
+  oneplus.innerHTML = counts[10] || 0
+  microsoft.innerHTML = counts[11] || 0
+  alienware.innerHTML = counts[12] || 0
+  xiaomi.innerHTML = counts[13] || 0
+  razer.innerHTML = counts[14] || 0
+  msi.innerHTML = counts[15] || 0
+  nvidia.innerHTML = counts[16] || 0
+  bose.innerHTML = counts[17] || 0
+  gopro.innerHTML = counts[18] || 0
+  fitbit.innerHTML = counts[19] || 0
+  canon.innerHTML = counts[20] || 0
+  nintendo.innerHTML = counts[21] || 0
+  sonos.innerHTML = counts[22] || 0
+  dji.innerHTML = counts[23] || 0
+  amazon.innerHTML = counts[24] || 0
+  asus.innerHTML = counts[25] || 0
+  jbl.innerHTML = counts[26] || 0
 }
